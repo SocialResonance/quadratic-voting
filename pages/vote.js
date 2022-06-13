f69d37813f962193a0fd79257847ba3f3722093b
alexrandaccio <alex@radicalxchange.org>
2022-06-12 19:50:50 -0500

    disable negative votes for wish list poll

--- pages/vote.js
+++ pages/vote.js
@@ -109,7 +109,11 @@ function Vote({ query }) {
       return current <= 0 ? true : canOccur;
     } else {
       // Or check for inverse state when subtracting
-      return current >= 0 ? true : canOccur;
+      if (data.event_data.event_title === "Wish List Poll") {
+        return (current >= 0 ? true : canOccur) && (current !== 0);
+      } else {
+        return current >= 0 ? true : canOccur;
+      }
     }
   };
 
